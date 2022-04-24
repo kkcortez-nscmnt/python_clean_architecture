c8e1bbab52f6e9e5dc05d277ca7d89d14e381df2
Jamilson do Nascimento <iero.eng.agricola@gmail.com>
2022-04-24 00:24:54 -0300

    chore: implementado domain,models

--- src/infra/repositories/user_repository.py
+++ src/infra/repositories/user_repository.py
@@ -1,7 +1,6 @@
-from collections import namedtuple
-
+from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
-from src.infra.entities import Users
+from src.infra.entities import Users as UsersModel
 
 
 class UserRepository:
@@ -15,15 +14,13 @@ class UserRepository:
         :return - nameduple InsertedData
         """
 
-        InsertedData = namedtuple("Users", "id name password")
-
         with DBConnectionHandler() as db_connection:
             try:
-                new_user = Users(name=name, password=password)
+                new_user = UsersModel(name=name, password=password)
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
 
-                return InsertedData(
+                return Users(
                     id=new_user.id, name=new_user.name, password=new_user.password
                 )
             except:
