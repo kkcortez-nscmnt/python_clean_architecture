6c74216cd7a85df957089a0b63184d2cd6f59d7a
Jamilson do Nascimento <iero.eng.agricola@gmail.com>
2022-04-26 13:59:54 -0300

    feat: adiciona select_user()

--- src/infra/repositories/user_repository.py
+++ src/infra/repositories/user_repository.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
 from src.infra.entities import Users as UsersModel
@@ -28,5 +30,54 @@ class UserRepository:
                 raise
             finally:
                 db_connection.session.close()
+
+        return None
+
+    def select_user(self, user_id: int = None, name: str = None) -> List[Users]:
+        """
+        Seleciona dados na entidade de usuario pelo id ou pelo nome
+        :Param - user_id: Id do registro
+               - name: User name
+        :return - Lista com os dados selecionados
+        """
+        try:
+            query_data = None
+
+            if user_id and not name:
+
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id)
+                        .one()
+                    )
+                    query_data = [data]
+
+            elif not user_id and name:
+
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(name=name)
+                        .one()
+                    )
+                    query_data = [data]
+
+            elif user_id and name:
+
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id, name=name)
+                        .one()
+                    )
+                    query_data = [data]
+            return query_data
+
+        except:
+            db_connection.session.rollback()
+            raise
+        finally:
+            db_connection.session.close()
 
         return None
